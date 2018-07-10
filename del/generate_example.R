83a1950d7be1eee37f1cfbbabf16b2f99d82dc03
devSJR <stefan.roediger@b-tu.de>
2018-07-10 17:15:32 +0200

    files deleted

--- del/generate_example.R
+++
@@ -1,8 +0,0 @@
-load("healthy_list.RData")
-
-pat67 <- healthy_list_fitc[37L:42]
-
-max_len <- max(lengths(pat67))
-
-write.csv(do.call(cbind, lapply(pat67, function(i) c(i, rep(NA, max_len - length(i))))), file = "example_counts.csv", 
-    row.names = FALSE)
