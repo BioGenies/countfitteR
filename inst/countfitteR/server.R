4e1f2468c4d14d3a4f39eb29b94a460c3e0f2554
jarochi <jaroslaw.chilimoniuk@gmail.com>
2018-07-12 10:41:46 +0200

    path fix

--- inst/countfitteR/server.R
+++ inst/countfitteR/server.R
@@ -8,7 +8,7 @@ library(pscl)
 
 setwd("../..")
 
-source("./R/load_all_func.R")
+source("R/load_all_func.R")
 
 options(DT.options = list(dom = "Brtip", buttons = c("copy", "csv", "excel", "print")))
 
