2af335229abaafe604c49015e0dbe9ad49bdfa27
devSJR <stefan.roediger@b-tu.de>
2018-07-13 14:32:18 +0200

    fix

--- R/countfitteR.R
+++ R/countfitteR.R
@@ -12,7 +12,11 @@
 #' @export countfitteR_gui
 #'
 #' @examples
-#' countfitter_gui()
+#' countfitteR_gui()
+
+#' @importFrom stats .getXlevels AIC binomial confint delete.response dnbinom dpois filter glm glm.fit lag logLik mad make.link median model.frame model.matrix model.response model.weights na.omit na.pass optim pnorm poisson predict printCoefmat quantile residuals sd terms update 
+#' @importFrom gridExtra combine
+#' @importFrom base intersect setdiff setequal union
 
 library(shiny)
 countfitteR_gui <- function(){
