86512559e7d9db8100f34361780f80e290734418
jarochi <jaroslaw.chilimoniuk@gmail.com>
2018-07-13 11:15:43 +0200

    ex

--- R/utils.R
+++ R/utils.R
@@ -1,8 +1,9 @@
-
+#' @export
 invlogit <- function(x) {
   1/(1 + exp(-x))
 }
 
+#' @export
 no_fit <- function() {
   list(fit = NA,
        coefficients = c(lambda = NA),
@@ -10,4 +11,5 @@ no_fit <- function() {
   )
 }
 
+#' @export
 nice_model_names <- c(pois = "Poisson", nb = "NB", zip = "ZIP", zinb = "ZINB")
