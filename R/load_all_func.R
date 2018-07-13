d1e778876513c806b7c9cc8eaf2ae9c139083457
jarochi <jaroslaw.chilimoniuk@gmail.com>
2018-07-13 08:33:43 +0200

    ...

--- R/load_all_func.R
+++ R/load_all_func.R
@@ -1,15 +1,15 @@
-# #' Loads all important functions
-# #' 
-# #' @name load_all_func
-# #' @export
+#' Loads all important functions
+#'
+#' @name load_all_func
+#'
 
-# load_all_func <- c('compare_fit.R', 'distrs.R',
-#                'fit_counts.R', 'fit_counts_separate.R', 'fit_counts_whole.R',
-#                'fitlist.R', 'ggtheme.R', 'process_counts.R',
-#                'utils.R', 'zeroinfl2.R')
-# 
-# sapply(load_all_func, function(i) source(paste0("./R/", i)))
+load_all_func <- c('compare_fit.R', 'distrs.R',
+               'fit_counts.R', 'fit_counts_separate.R', 'fit_counts_whole.R',
+               'fitlist.R', 'ggtheme.R', 'process_counts.R',
+               'utils.R', 'zeroinfl2.R')
+
+sapply(load_all_func, function(i) source(paste0("./R/", i)))
 # 
 # save.image("./data/laf.RData")
 
-load("./data/laf.RData")
+# load("./data/laf.RData")
