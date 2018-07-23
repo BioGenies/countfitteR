3047047dd581bb32b827e559408a583efb259f96
jarochi <jaroslaw.chilimoniuk@gmail.com>
2018-07-23 12:41:28 +0200

    fix

--- R/load_all_func.R
+++ R/load_all_func.R
@@ -1,4 +1,4 @@
-# Loads all important functions
+## Loads all important functions
 
 # load_all_func <- c('compare_fit.R', 'distrs.R',
 #                'fit_counts.R', 'fit_counts_separate.R', 'fit_counts_whole.R',
@@ -6,6 +6,9 @@
 #                'utils.R', 'zeroinfl2.R')
 # 
 # sapply(load_all_func, function(i) source(paste0("./R/", i)))
+
+
+## create rdata with all functions, easier load
 
 # save.image("./data/laf.RData")
 
