4e6a4bfe4be3a3859f8a2d0a246a29eef8500073
jarochi <jaroslaw.chilimoniuk@gmail.com>
2018-08-07 17:17:04 +0200

    fix

--- R/datasets.R
+++ R/datasets.R
@@ -33,7 +33,7 @@ NULL
 NULL
 
 #' @name foci_count_APC
-#' @title example data extracted from Aklidus system. Files with only APC fluorescent dye were merged.
+#' @title example data extracted from Aklidus system. Counts with only APC fluorescent dye were merged.
 #' @description Data of six bad replicates exported from \code{foci_count_APC}
 #' @docType data
 #' @usage foci_count_APC
@@ -41,15 +41,16 @@ NULL
 NULL
 
 #' @name foci_count_FITC
-#' @title example data extracted from Aklidus system. Files with only FITC fluorescent dye were merged.
+#' @title example data extracted from Aklidus system. Counts with only FITC fluorescent dye were merged.
 #' @description Data of six bad replicates exported from \code{foci_count_FITC}
 #' @docType data
 #' @usage foci_count_FITC
 #' @keywords datasets
 NULL
 
-#' @name foci_count_merged
-#' @title example data extracted from Aklidus system and merged into one file
+#' @name foci_count
+#' @title example data extracted from Aklidus system and merged into one file. Counts in this file will not fit properly, 
+#' due to the fact that we integrated into the file counts with two different fluorescent dyes used. 
 #' @description Data of six bad replicates exported from \code{foci_count_merged}
 #' @docType data
 #' @usage foci_count_merged
