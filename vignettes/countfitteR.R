c17c1637c4e2581efa6efb312608df4c69b4ce65
michbur <michalburdukiewicz@gmail.com>
2018-08-09 16:17:23 +0200

    removed unnecessary files

--- vignettes/countfitteR.R
+++
@@ -1,7 +0,0 @@
-## ----eval=TRUE, echo=TRUE------------------------------------------------
-# Load countfitteR package
-library(countfitteR)
-
-# Assign the csv file to filename
-# df <- read.csv(system.file("example_counts.csv", package = "countfitteR"))
-
