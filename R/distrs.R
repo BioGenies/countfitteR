5a24450a764bf35a90741d69c4ce2e77af8af5c1
michbur <michalburdukiewicz@gmail.com>
2018-08-11 21:35:33 +0200

    fixing documentation

--- R/distrs.R
+++ R/distrs.R
@@ -5,7 +5,8 @@
 #' @description  Density and random generation for the zero inflated Poisson distribution.
 #' @param x vector of (non-negative integer) quantiles.
 #' @param lambda vector of (non-negative) means.
-#' @param p probability of zero
+#' @param p probability of zeros.
+#' @param n number of random values to return.
 #' @examples
 #' df <- data.frame(poisson = rpois(25, 0.3), binomial = rbinom(25, 1, 0.8))
 #' compare_fit(df, fitlist = fit_counts(df, model = "all"))
