28f409a31d2076d3401d8ab51822c633d50a8558
Michał Burdukiewicz <michalburdukiewicz@gmail.com>
2020-09-01 20:40:42 +0200

    improved gui call

--- R/countfitteR_gui.R
+++ R/countfitteR_gui.R
@@ -7,11 +7,19 @@
 #' @keywords count Poisson zero-inflated
 #' @export countfitteR_gui
 #' @author Jaroslaw Chilimoniuk, Stefan Roediger, Michal Burdukiewcz
+#' @importFrom tools package_dependencies
 #' @examples 
 #' if(interactive()) { 
 #'   countfitteR_gui()
 #' }
 
 countfitteR_gui <- function(){
+  countfitteR_suggests <- package_dependencies("countfitteR", 
+                                               which = "Suggests")[["countfitteR"]]
+  
+  if(!all(countfitteR_suggests %in% rownames(available.packages()))) {
+    stop(paste0("The countfitteR GUI requires following packages: ", 
+                paste0(countfitteR_suggests, collapse = ", ")))
+  }
   runApp(system.file("countfitteR", package = "countfitteR"))
 }
